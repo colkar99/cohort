d6f7e0e8cbf577dd436a7c6afd69ee54684b629e
colkar99 <colkar99@gmail.com>
2019-02-04 17:18:23 +0530

    asdas

--- app/controllers/v1/founding_sources_controller.rb
+++ app/controllers/v1/founding_sources_controller.rb
@@ -8,7 +8,7 @@ module V1
 			startup_auth = startup_auth_check(params[:founding_source][:startup_profile_id],current_user)
 			if startup_auth
 				if founding_source.save!
-					startup_profile = StartupProfile.find(params[:founding_source][:startup_profile_id])
+					startup_profile = StartupProfile.find(founding_source.startup_profile_id)
 					founding_sources = startup_profile.founding_sources
 					render json:  founding_sources , status: :created
 				else
