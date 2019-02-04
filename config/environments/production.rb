bd2a987ff8d4ddb0e5054c6bff28295b6ea3854a
colkar99 <colkar99@gmail.com>
2019-02-04 11:27:20 +0530

    casda

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
     domain: 'example.com',
     authentication: "plain",
     # enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"] ,
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: 'colkar99@gmail.com ' ,
+    password: 'colkar99'
   }
   config.action_mailer.default_url_options = {host: "localhost:3000"}
 end
