44dc7f7bc892a08c34321e3c9e0cf535d3caf82c
colkar99 <colkar99@gmail.com>
2019-02-06 14:42:07 +0530

    cors edites

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-      origins ['http://localhost:4200','http://colkar.tk/']
+      # origins ['http://localhost:4200','http://colkar.tk/']
+      origins '*'
 
     resource '*',
       headers: :any,
