cdcf71425139e2316bce63721a58af821988d4df
colkar99 <colkar99@gmail.com>
2019-03-06 14:51:50 +0530

    asas

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id ,:first_name,:last_name,:full_name,:email,:user_main_image,:access_token,:phone_number,:user_type,:roles,:is_first_time_logged_in,:startup_profile_name,:facebook_link,:linkedin_link,:skype_id,:other_links
+  attributes :id ,:first_name,:last_name,:full_name,:email,:user_main_image,:access_token,
+  :phone_number,:user_type,:roles,:is_first_time_logged_in,:startup_profile_name,:facebook_link,
+  :linkedin_link,:skype_id,:other_links,:credentials,:commitment,:address_line_1,:address_line_2,:city,
+  :state_province_region,:zip_pincode_postalcode,:country,:designation
   	has_one :mentor_user
 
   def roles
@@ -10,3 +13,4 @@ class UserSerializer < ActiveModel::Serializer
   	object.startup_profiles.first.try(:startup_name)
   end
 end
+
