eda9321262802acec7b82b89ea7c7d17238066c5
colkar99 <colkar99@gmail.com>
2019-03-21 16:41:04 +0530

    asas

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,6 +65,6 @@ Rails.application.configure do
   }
   config.action_mailer.default_url_options = {host: "localhost:3000"}
   # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /file:\/\/*/, 'file://']
-  config.action_cable.disable_request_forgery_protection = true
+  # config.action_cable.disable_request_forgery_protection = true
 
 end
