598ca9616e236226febc92846f1c9bb8178264e9
colkar99 <colkar99@gmail.com>
2019-03-21 16:03:23 +0530

    asas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,7 @@ Rails.application.configure do
     password: ENV['GMAIL_PASSWORD']
   }
   config.action_mailer.default_url_options = {host: "localhost:3000"}
-    config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /file:\/\/*/, 'file://']
+    # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /file:\/\/*/, 'file://']
+      config.action_cable.disable_request_forgery_protection = true
 
 end
