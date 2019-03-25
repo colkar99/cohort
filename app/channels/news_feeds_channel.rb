1d8a54f71555abbe0669af4ae18a27a586cb94ce
colkar99 <colkar99@gmail.com>
2019-03-25 17:01:47 +0530

    asas

--- app/channels/news_feeds_channel.rb
+++ app/channels/news_feeds_channel.rb
@@ -2,4 +2,13 @@ class NewsFeedsChannel < ApplicationCable::Channel
 	def subscribed
 		stream_from "news_feeds_channel"
 	end
+
+	def unsubscribed
+		      chat_request_chanel_token = "What to put here????"
+
+      message = "Client disconnected"  
+
+      ActionCable.server.broadcast "chat_request_#{chat_request_chanel_token}_channel",
+                                   disconnected: message
+	end
 end
